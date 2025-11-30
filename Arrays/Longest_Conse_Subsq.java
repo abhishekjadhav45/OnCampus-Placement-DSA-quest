ab0ebe523e903910090da8fb7391f6a82f11885c
Student abhishek45 <abhijadhav0619@gmail.com>
2025-11-30 22:47:57 +0530

    Add Longest Consecutive Subsequence solution

---
+++ Arrays/Longest_Conse_Subsq.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        // put all elemnts in HashSet
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for(int num : set){
+
+        if(!set.contains(num - 1)){
+
+            int currNum = num;
+            int count = 1;
+
+            
+
+            while( set.contains( currNum + 1)){
+
+                currNum++;
+                count++;
+
+            }
+
+            longest = Math.max(longest , count);
+
+        }
+        }
+        return longest;
+    }
+}
