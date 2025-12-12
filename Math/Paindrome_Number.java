f7a7e56319553d824fbd6a9d52208abd1554af11
Student abhishek45 <abhijadhav0619@gmail.com>
2025-12-12 23:25:00 +0530

    Update Paindrome_Number.java with improved formatting and comments

--- Math/Paindrome_Number.java
+++ Math/Paindrome_Number.java
@@ -1,23 +1,20 @@
-// LeetCode 9 - Palindrome Number
+// LeetCode 9 // 
 
 class Solution {
     public boolean isPalindrome(int x) {
-        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if(x < 0 || (x % 10 == 0  && x != 0)){
             return false;
         }
-        
         int reversed = 0;
         int original = x;
 
-        // Reverse the number
-        while (x > 0) {
+          while(x>0){
             int digit = x % 10;
             reversed = reversed * 10 + digit;
-            x = x / 10;
-        }
-
-        // Check if original and reversed are equal
-        return original == reversed;
+            x = x  / 10;
+          }
+    
+          return original == reversed;
+        
     }
 }
