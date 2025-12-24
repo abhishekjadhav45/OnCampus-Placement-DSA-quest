93ea0d6cdec8ddb456f4bf124c6f2fb642cddd77
Student abhishek45 <abhijadhav0619@gmail.com>
2025-12-24 21:11:58 +0530

    Add Coin Change 2 solution

---
+++ Dynamic_Programming/Coin_Change_2.java
@@ -0,0 +1,23 @@
+// LeetCode  // 
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        int[] dp = new int[amount + 1];
+
+        // base case 
+
+        dp[0] = 1;  // 0 coins wwe can use 0 to make it 
+
+        // for each coins 
+
+        for(int coin : coins){
+
+            for(int i=coin ; i<= amount ; i++) {
+
+                dp[i] = dp[i] + dp[i-coin]; // i-1 means the total ways to complete the amount was i-1 supp amt = 5 , 1 = 5 c = 1 than i-c = 4 how amy ways to compete 4
+
+            }
+        }
+        return dp[amount];
+    }
+}
